8aa8610d6ec9db04eb5d5a4523b43d2ac54bcecc
calicanadian <ryan.stuhl@gmail.com>
2019-08-29 10:03:25 -0700

    CH-2 Add get_population method to calculate population between decades

--- app/models/population.rb
+++ app/models/population.rb
@@ -8,22 +8,38 @@ class Population < ApplicationRecord
     Population.all.map(&:year).max.year
   end
 
+  def self.get_population(year)
+    year1 = (year - year%10)
+    year2 = (year - year%10) + 10
+    pop1 = Population.find_by_year(Date.new(year1)).population
+    pop2 = Population.find_by_year(Date.new(year2)).population
+    if year2 > max_year
+      # modify the modulo 10 of the year to a decimal for population multiplication
+      population = (pop1 * (year%10 * 0.1).round(1)) + pop1
+    else
+      # modify the modulo 10 of the year to a decimal for population multiplication
+      population = ((pop2 - pop1) * (year%10 * 0.1).round(1)) + pop1
+    end
+    return population.round
+  end
+
   def self.get(year)
     year = year.to_i
 
     return 0 if year < min_year
-    if year > max_year
-      pop = Population.find_by_year(Date.new(max_year))
-      return pop.population
+    return Population.find_by_year(Date.new(year)).population if year == min_year
+    if year >= max_year
+      pop = Population.find_by_year(Date.new(max_year)).population
+      return pop
     end
 
     pop = nil
     until pop
-      pop = Population.find_by_year(Date.new(year))
+      pop = get_population(year)
       year = year - 1
     end
 
-    return pop.population if pop
+    return pop if pop
 
     nil
   end
